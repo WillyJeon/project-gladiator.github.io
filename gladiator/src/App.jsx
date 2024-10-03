faac42628df2ae53386610581750b7d5441f795a
William Jeon <wj3482@g.rit.edu>
2024-10-02 20:01:12 -0400

    Run stuff

---
+++ gladiator/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { FaLinkedin } from "react-icons/fa";
+import './App.css'
+import './css/main.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='flex flex-col items-center'>
+      <h1 className='text-xl underline font-bold'>Hello RIOT GAMES</h1>
+        <h1 className='text-xl underline font-bold text-center'>PLEASE HIRE ME PLEASE PLEASE PLEASE</h1>
+        <FaLinkedin  className='text-[200px] '/>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
